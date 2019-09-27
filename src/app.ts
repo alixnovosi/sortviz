d4d79fe11e4f763a5e6fb62631351979f40716e4
Andrew Michaud <dev@drew.life>
2019-09-26 22:56:15 -0700

    init commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import "./styles/main.scss";
+
+class App {
+
+    constructor() {
+    }
+
+    public setup(): void {
+    }
+
+    private initCanvas(): void {
+    }
+}
+
+window.onload = () => {
+    let app = new App();
+
+    app.setup();
+}
